ea691165cc9e456d02b9f148ab571f10ed70aa5f
David Baumgold <david@davidbaumgold.com>
2020-04-14 00:05:57 +0200

    first pass at implementing basic functionality

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,96 @@
 import { getInput, setOutput, setFailed, debug } from "@actions/core";
-// import { wait } from "./wait";
+import { GitHub, context } from "@actions/github";
+import { WebhookPayloadPullRequest } from "@octokit/webhooks";
+import { HttpClient } from "@actions/http-client";
+import { ClubhouseMember, ClubhouseProject, ClubhouseStory } from "./types";
+
+async function getClubhouseUserId(
+  githubUsername: string,
+  http: HttpClient
+): Promise<string | undefined> {
+  const CLUBHOUSE_TOKEN: string = getInput("clubhouse-token");
+  const GITHUB_TOKEN: string = getInput("github-token");
+
+  const membersResponse = await http.getJson<ClubhouseMember[]>(
+    `https://api.clubhouse.io/api/v3/members?token=${CLUBHOUSE_TOKEN}`
+  );
+  if (!membersResponse.result) {
+    setFailed("Clubhouse API failure: /members");
+    return;
+  }
+  const emailToClubhouseId = membersResponse.result.reduce((e2id, member) => {
+    const email = member.profile.email_address;
+    const clubhouseId = member.id;
+    if (email) {
+      e2id.set(email, clubhouseId);
+    }
+    return e2id;
+  }, new Map<string, string>());
+  debug(JSON.stringify(emailToClubhouseId));
+
+  const octokit = new GitHub(GITHUB_TOKEN);
+  const userResponse = await octokit.users.getByUsername({
+    username: githubUsername,
+  });
+  const user = userResponse.data;
+  if (user.email) {
+    return emailToClubhouseId.get(user.email);
+  }
+}
+
+async function getClubhouseProjectId(
+  projectName: string,
+  http: HttpClient
+): Promise<number | undefined> {
+  const CLUBHOUSE_TOKEN: string = getInput("clubhouse-token");
+
+  const projectsResponse = await http.getJson<ClubhouseProject[]>(
+    `https://api.clubhouse.io/api/v3/projects?token=${CLUBHOUSE_TOKEN}`
+  );
+  if (!projectsResponse.result) {
+    setFailed("Clubhouse API failure: /projects");
+    return;
+  }
+  const projectNameToClubhouseId = projectsResponse.result.reduce(
+    (p2id, project) => {
+      p2id.set(project.name, project.id);
+      return p2id;
+    },
+    new Map<string, number>()
+  );
+  debug(JSON.stringify(projectNameToClubhouseId));
+
+  return projectNameToClubhouseId.get(projectName);
+}
 
 async function run(): Promise<void> {
+  if (context.eventName !== "pull_request") {
+    debug("This action only works with `pull_request` events");
+    return;
+  }
+  const payload = context.payload as WebhookPayloadPullRequest;
   try {
-    const ms: string = getInput("milliseconds");
-    debug(`Waiting ${ms} milliseconds ...`);
+    const http = new HttpClient();
+    const CLUBHOUSE_TOKEN: string = getInput("clubhouse-token");
+    const PROJECT_NAME: string = getInput("project-name");
+    const githubUsername = payload.pull_request.user.login;
+    const clubhouseUserId = await getClubhouseUserId(githubUsername, http);
+    const clubhouseProjectId = await getClubhouseProjectId(PROJECT_NAME, http);
 
-    debug(new Date().toTimeString());
-    // await wait(parseInt(ms, 10));
-    debug(new Date().toTimeString());
-
-    setOutput("time", new Date().toTimeString());
+    const storyResponse = await http.postJson<ClubhouseStory>(
+      `https://api.clubhouse.io/api/v3/stories?token=${CLUBHOUSE_TOKEN}`,
+      {
+        name: payload.pull_request.title,
+        description: payload.pull_request.body,
+        owner_ids: [clubhouseUserId],
+        project_id: clubhouseProjectId,
+      }
+    );
+    if (!storyResponse.result) {
+      setFailed("Clubhouse API failure: /stories");
+      return;
+    }
+    setOutput("story-id", storyResponse.result.id.toString());
   } catch (error) {
     setFailed(error.message);
   }
---
+++ src/types.ts
@@ -0,0 +1,297 @@
+/** Icons are used to attach images to Organizations, Members, and Loading screens in the Clubhouse web application. */
+export interface ClubhouseUserIcon {
+  entity_type: "user-icon";
+  id: string;
+  created_at: string;
+  updated_at: string;
+  url: string;
+}
+
+/** A group of Member profile details. */
+export interface ClubhouseMemberProfile {
+  entity_type: "profile";
+  id: string;
+  deactivated: boolean;
+  two_factor_auth_activated: boolean;
+  mention_name: string;
+  name: string | null;
+  gravatar_hash: string | null;
+  display_icon: ClubhouseUserIcon | null;
+  email_address: string | null;
+}
+
+/** Details about individual Clubhouse user within the Clubhouse organization that has issued the token. */
+export interface ClubhouseMember {
+  entity_type: "member";
+  id: string;
+  created_at: string;
+  updated_at: string;
+  role: string;
+  disabled: boolean;
+  created_without_invite: false;
+  group_ids: string[];
+  profile: ClubhouseMemberProfile;
+}
+
+/** A group of calculated values for this Project. */
+export interface ClubhouseProjectStats {
+  num_points: number;
+  num_related_documents: number;
+  num_stories: number;
+}
+
+/** Projects typically map to teams (such as Frontend, Backend, Mobile, Devops, etc) but can represent any open-ended product, component, or initiative. */
+export interface ClubhouseProject {
+  entity_type: "project";
+  abbreviation: string | null;
+  app_url: string;
+  archived: boolean;
+  color: string | null;
+  created_at: string | null;
+  days_to_thermometer: number;
+  description: string | null;
+  external_id: string | null;
+  follower_ids: string[];
+  id: number;
+  iteration_length: number;
+  name: string;
+  show_thermometer: boolean;
+  start_time: string;
+  stats: ClubhouseProjectStats;
+  team_id: number;
+  updated_at: string | null;
+}
+
+/** Branch refers to a GitHub branch. Branches are feature branches associated with Clubhouse Stories. */
+export interface ClubhouseBranch {
+  entity_type: "branch";
+  created_at: string | null;
+  deleted: boolean;
+  id: number | null;
+  merged_branch_ids: number[];
+  name: string;
+  persistent: boolean;
+  pull_requests: ClubhousePullRequest[];
+  repository_id: number | null;
+  updated_at: string | null;
+  url: string;
+}
+
+/** A Comment is any note added within the Comment field of a Story. */
+export interface ClubhouseComment {
+  entity_type: "comment";
+  app_url: string;
+  author_id: string | null;
+  created_at: string;
+  external_id: string | null;
+  group_mention_ids: string[];
+  id: number;
+  member_mention_ids: string[];
+  position: number;
+  story_id: number;
+  text: string;
+  updated_at: string | null;
+}
+
+/** The Identity of the GitHub user that authored the Commit. */
+export interface ClubhouseIdentity {
+  entity_type: "identity";
+  name: string | null;
+  type: "github";
+}
+
+/** Commit refers to a GitHub commit and all associated details. */
+export interface ClubhouseCommit {
+  author_email: string;
+  author_id: string | null;
+  author_identity: ClubhouseIdentity;
+  created_at: string;
+  entity_type: "commit";
+  hash: string;
+  id: number | null;
+  merged_branch_ids: number[];
+  message: string;
+  repository_id: number | null;
+  timestamp: string;
+  updated_at: string | null;
+  url: string;
+}
+
+/** A External Ticket allows you to create a link between an external system, like Zendesk, and a Clubhouse story. */
+export interface ClubhouseExternalTicket {
+  epic_ids: number[];
+  external_id: string;
+  external_url: string;
+  id: string;
+  story_ids: number[];
+}
+
+/** A File is any document uploaded to your Clubhouse. Files attached from a third-party service can be accessed using the Linked Files endpoint. */
+export interface ClubhouseFile {
+  content_type: string;
+  created_at: string;
+  description: string | null;
+  entity_type: "file";
+  external_id: string | null;
+  filename: string;
+  group_mention_ids: string[];
+  id: number;
+  member_mention_ids: string[];
+  name: string;
+  size: number;
+  story_ids: number[];
+  thumbnail_url: string | null;
+  updated_at: string | null;
+  uploader_id: string;
+  url: string | null;
+}
+
+/** A Label can be used to associate and filter Stories and Epics, and also create new Workspaces. */
+export interface ClubhouseLabel {
+  app_url: string;
+  archived: boolean;
+  color: string | null;
+  created_at: string | null;
+  description: string | null;
+  entity_type: "label";
+  external_id: string | null;
+  id: number;
+  name: string;
+  stats: ClubhouseLabelStats;
+  updated_at: string | null;
+}
+
+/** A group of calculated values for this Label. */
+export interface ClubhouseLabelStats {
+  num_epics: number;
+  num_points_completed: number;
+  num_points_in_progress: number;
+  num_points_total: number;
+  num_related_documents: number;
+  num_stories_completed: number;
+  num_stories_in_progress: number;
+  num_stories_total: number;
+  num_stories_unestimated: number;
+}
+
+/** The stats object for Stories. */
+export interface ClubhouseStoryStats {
+  num_related_documents: number;
+}
+
+/** Linked files are stored on a third-party website and linked to one or more Stories. Clubhouse currently supports linking files from Google Drive, Dropbox, Box, and by URL. */
+export interface ClubhouseLinkedFile {
+  content_type: string | null;
+  created_at: string;
+  description: string | null;
+  entity_type: "linked_file";
+  group_mention_ids: string[];
+  id: number;
+  member_mention_ids: number;
+  name: string;
+  size: number | null;
+  story_ids: number[];
+  thumbnail_url: string | null;
+  type: string;
+  updated_at: string;
+  uploader_id: string;
+  url: string;
+}
+
+/** Corresponds to a GitHub Pull Request attached to a Clubhouse story. */
+export interface ClubhousePullRequest {
+  branch_id: number;
+  branch_name: string;
+  closed: boolean;
+  created_at: string;
+  entity_type: "pull_request";
+  id: number;
+  num_added: number;
+  num_commits: number | null;
+  num_modified: number | null;
+  num_removed: number;
+  number: number;
+  target_branch_id: number;
+  target_branch_name: string;
+  title: string;
+  updated_at: string;
+  url: string;
+}
+
+/** The type of Story Link. The string can be subject or object.  */
+export interface ClubhouseTypedStoryLink {
+  created_at: string;
+  entity_type: "typed_story_link";
+  id: number;
+  object_id: number;
+  subject_id: number;
+  type: string;
+  updated_at: string;
+  verb: string;
+}
+
+export interface ClubhouseTask {
+  complete: boolean;
+  completed_at: string | null;
+  created_at: string;
+  description: string;
+  entity_type: "task";
+  external_id: string | null;
+  group_mention_ids: string[];
+  id: number;
+  member_mention_ids: string[];
+  owner_ids: string[];
+  position: number;
+  story_id: number;
+  updated_at: string | null;
+}
+
+/** Stories are the standard unit of work in Clubhouse and represent individual features, bugs, and chores. */
+export interface ClubhouseStory {
+  entity_type: "story";
+  app_url: string;
+  archived: boolean;
+  blocked: boolean;
+  blocker: boolean;
+  branches: ClubhouseBranch[];
+  comments: ClubhouseComment[];
+  commits: ClubhouseCommit[];
+  completed: boolean;
+  completed_at: string | null;
+  completed_at_override: string | null;
+  created_at: string;
+  cycle_time: number;
+  deadline: string | null;
+  description: string;
+  epic_id: number | null;
+  estimate: number | null;
+  external_id: string | null;
+  external_tickets: ClubhouseExternalTicket[];
+  files: ClubhouseFile[];
+  follower_ids: string[];
+  group_mention_ids: string[];
+  id: number;
+  iteration_id: number | null;
+  labels: ClubhouseLabel[];
+  lead_time: number;
+  linked_files: ClubhouseLinkedFile[];
+  member_mention_ids: string[];
+  mention_ids: string[];
+  moved_at: string | null;
+  name: string;
+  owner_ids: string[];
+  position: number;
+  previous_iteration_ids: number[];
+  project_id: number;
+  pull_requests: ClubhousePullRequest[];
+  requested_by_id: string;
+  started: boolean;
+  started_at: string | null;
+  started_at_override: string | null;
+  stats: ClubhouseStoryStats;
+  story_links: ClubhouseTypedStoryLink[];
+  story_type: "feature" | "bug" | "chore";
+  tasks: ClubhouseTask[];
+  updated_at: string | null;
+  workflow_state_id: number;
+}
